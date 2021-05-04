2ab6edbd7842933803e8b0f0f1fc6f9b470f634f
lina0624 <lina.park624@gmail.com>
2021-05-04 13:16:48 +0900

    2021-05-04 first file

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+package java_20210429;
+
+
+//폰트바꾸는 방법
+//window >preferences >font쓰기 >java >java text editor font
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
